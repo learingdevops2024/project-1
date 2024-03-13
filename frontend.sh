989575e293f601d444c1efb108822f9e604e2f58
santhoshi <santhoshi6april@gmail.com>
2024-03-13 21:33:08 +0530

    frontend script

--- frontend.sh
+++ frontend.sh
@@ -5,6 +5,15 @@ rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+vim /etc/nginx/default.d/expense.conf
+proxy_http_version 1.1;
+
+location /api/ { proxy_pass http:// 172.31.11.193 :8080/; }
+
+location /health {
+  stub_status on;
+  access_log off;
+}
 systemctl restart nginx
 
 
