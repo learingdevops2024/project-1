2c48496979c58da12a98d055c6210f6aeae05afb
santhoshi <santhoshi6april@gmail.com>
2024-03-13 21:39:30 +0530

    frontend script space bar changes

--- frontend.sh
+++ frontend.sh
@@ -7,9 +7,7 @@ cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 vim /etc/nginx/default.d/expense.conf
 proxy_http_version 1.1;
-
 location /api/ { proxy_pass http:// 172.31.11.193 :8080/; }
-
 location /health {
   stub_status on;
   access_log off;
