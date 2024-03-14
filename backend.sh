2d6ce76cc04967b0318f05729d849439ee28f772
santhoshi <santhoshi6april@gmail.com>
2024-03-14 11:58:58 +0530

    removing the old content..//... 22nd line error

--- backend.sh
+++ backend.sh
@@ -19,4 +19,5 @@ systemctl enable backend
 systemctl start backend
 
 dnf install mysql -y
-mysql -h 172.31.12.86  -uroot -pExpenseApp@1 < /app/schema/backend.service
+mysql -h 172.31.12.86 -uroot -pExpenseApp@1 < /app/schema/backend.sql
+
