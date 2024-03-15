5036118d593c5b99f2a563f8f504d8c6a941e93f
santhoshi <santhoshi6april@gmail.com>
2024-03-15 19:20:28 +0530

    no space

--- backend.sh
+++ backend.sh
@@ -1,9 +1,12 @@
+print_Task_Heading "disable enable intall nodejs"
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
-
+print_Task_Heading "adding app user"
 useradd expense
+print_Task_Heading "copy backend serive file"
 cp backend.service /etc/systemd/system/backend.service
+print_Task_Heading "removing "
 rm -rf /app
 mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
