484b4a1b6c6adc063e6773a17c640c85902c0df5
santhoshi <santhoshi6april@gmail.com>
2024-03-15 17:35:01 +0530

    backend v2 mistake

--- backend.sh
+++ backend.sh
@@ -9,11 +9,10 @@ mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
 
 cd/app
-unzip /tmp/backend.zip
+unzip /tmp/backend-v2.zip
 cd/app
 
 npm install
-
 
 
 systemctl daemon-reload
